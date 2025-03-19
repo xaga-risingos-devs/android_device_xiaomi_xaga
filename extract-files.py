fc3118f938bef95a3a8627989244ec5a6f303534
Nauval Rizky <enuma.alrizky@gmail.com>
2025-03-19 10:57:56 +0100

    xaga: Restrict `mi_thermald` from CPU hotplugging
    * hotplug data will be parsed but not applied to the node.
    
    Change-Id: If5664212142f5bb3a1e8c937b7054f51a1055361

--- extract-files.py
+++ extract-files.py
@@ -28,6 +28,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/mi_thermald': blob_fixup()
+        .binary_regex_replace(b'%d/on', b'%d/..'),
     'vendor/etc/sensors/hals.conf': blob_fixup()
         .regex_replace('android.hardware.sensors@2.X-subhal-mediatek.so', 'android.hardware.sensors@2.0-subhal-impl-1.0.so')
         .regex_replace('sensors.touch.detect.so', 'sensors.dynamic_sensor_hal.so'),
