76db39ee03f94989b3e60ed851d6e5e6a86410de
Hecheng Yu <kde-yyds@qq.com>
2025-04-06 11:05:38 +0800

    xaga: Add dolby blobs

--- extract-files.py
+++ extract-files.py
@@ -47,7 +47,9 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/mt6895/libmtkcam_stdutils.so', 'vendor/lib64/hw/mt6895/android.hardware.camera.provider@2.6-impl-mediatek.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
     ('vendor/lib64/lib3a.ae.pipe.so', 'vendor/lib64/libteei_daemon_vfs.so', 'vendor/lib64/mt6895/libaaa_toneutil.so', 'vendor/lib64/mt6895/lib3a.flash.so', 'vendor/lib64/mt6895/lib3a.sensors.color.so', 'vendor/lib64/mt6895/lib3a.sensors.flicker.so'): blob_fixup()
-        .add_needed('liblog.so')
+        .add_needed('liblog.so'),
+    ('vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service'): blob_fixup()
+	.add_needed('libstagefright_foundation-v33.so')
 }  # fmt: skip
 
 module = ExtractUtilsModule(
